c40d56cc66b2b1f6feb0ae6964385eebcd67c1ed
bai21018 <97064556+bai21018@users.noreply.github.com>
2022-02-05 22:02:12 -0700

    Commerce CSS updates/color schemes

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -6,3 +6,21 @@ function navinator() {
           x.className = "topnav";
         }
       }
+
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getDay();
+console.log(dayNumber);
+
+
+const element = document.getElementById("message_banner");
+if (dayNumber == 1) {
+  element.classList.add("showme");
+} 
+else if(dayNumber ==2) {
+  element.classList.add("showme");
+}
+else {
+  element.classList.add("hideme");
+}
