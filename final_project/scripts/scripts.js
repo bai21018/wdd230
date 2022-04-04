026fa396ed3d631f4613e0144d41b33d0b48192b
bai21018 <97064556+bai21018@users.noreply.github.com>
2022-04-04 06:47:27 -0600

    Favorite Button MK I

--- final_project/scripts/scripts.js
+++ final_project/scripts/scripts.js
@@ -59,12 +59,13 @@ function display_temples_entry(i, dat){
   document.getElementById("temple_closures" + i).innerHTML = dat.temples[i].closures;
 }
 
-function likeIt() {
+/*function likeIt() {
   if (typeof(Storage) !== "undefined") {
     if (sessionStorage.clickcount) {
       sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
       alert(sessionStorage.clickcount);
       //toggle goes here
+      
       //has already been liked...
     } else {
       //never been liked...
@@ -73,7 +74,7 @@ function likeIt() {
   } else {
     document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
   }
-}
+}*/
 
 fetch('https://raw.githubusercontent.com/bai21018/wdd230/e318525e9fae47b1cf30823d3492a18d13091a96/final_project/temples.json')
   .then(result => result.json())
@@ -82,3 +83,154 @@ fetch('https://raw.githubusercontent.com/bai21018/wdd230/e318525e9fae47b1cf30823
       display_temples_entry(i, output);
    }
   });
+
+
+
+/* ==== LIKE BUTTON ==== */
+function buttonState () {
+  var currentClick = localStorage.clickcount++;
+  var buttonToggle = document.getElementById("favoriteButton");
+  console.log(currentClick)
+  if(currentClick % 2 == 0) {
+  buttonToggle.innerHTML = "Favorite";
+  document.getElementById("favoriteButton").className = "not_favorited";
+}
+else {
+  buttonToggle.innerHTML = "Favorited!";
+  document.getElementById("favoriteButton").className = "favorited";
+}}
+
+function loadButtonState () {
+  var currentClick = localStorage.clickcount;
+  var buttonToggle = document.getElementById("favoriteButton");
+  if(currentClick % 2 == 0) {
+    buttonToggle.innerHTML = "Favorited!";
+  document.getElementById("favoriteButton").className = "favorited";
+}
+else {
+  buttonToggle.innerHTML = "Favorite";
+  document.getElementById("favoriteButton").className = "not_favorited";
+}}
+
+
+/* ==== MN BUTTON ==== */
+function buttonStateMN () {
+  var currentClick = localStorage.clickcount++;
+  var buttonToggle = document.getElementById("favoriteButtonMN");
+  console.log(currentClick)
+  if(currentClick % 2 == 0) {
+  buttonToggle.innerHTML = "Favorite";
+  document.getElementById("favoriteButtonMN").className = "not_favorited";
+}
+else {
+  buttonToggle.innerHTML = "Favorited!";
+  document.getElementById("favoriteButtonMN").className = "favorited";
+}}
+
+function loadButtonStateMN () {
+  var currentClick = localStorage.clickcount;
+  var buttonToggle = document.getElementById("favoriteButtonMN");
+  if(currentClick % 2 == 0) {
+    buttonToggle.innerHTML = "Favorited!";
+  document.getElementById("favoriteButtonMN").className = "favorited";
+}
+else {
+  buttonToggle.innerHTML = "Favorite";
+  document.getElementById("favoriteButtonMN").className = "not_favorited";
+}}
+
+
+/* ==== IL BUTTON ==== */
+/*function buttonStateIL () {
+  var currentClick =+ localStorage.clickcountIL++;
+  var buttonToggle = document.getElementById("favoriteButtonIL");
+  if(currentClick % 2 == 0) {
+  buttonToggle.innerHTML = "Favorite";
+  document.getElementById("favoriteButtonIL").className = "not_favorited";
+}
+else {
+  buttonToggle.innerHTML = "Favorited!";
+  document.getElementById("favoriteButtonIL").className = "favorited";
+}}
+
+function loadButtonState () {
+  var currentClick = localStorage.clickcount;
+  var buttonToggle = document.getElementById("favoriteButtonIL");
+  if(currentClick % 2 == 0) {
+    buttonToggle.innerHTML = "Favorited!";
+  document.getElementById("favoriteButtonIL").className = "favorited";
+}
+else {
+  buttonToggle.innerHTML = "Favorite";
+  document.getElementById("favoriteButtonIL").className = "not_favorited";
+}}*/
+
+
+/* ==== TN BUTTON ==== */
+/*function buttonStateTN () {
+  var currentClick =+ localStorage.clickcountTN++;
+  var buttonToggle = document.getElementById("favoriteButtonTN");
+  if(currentClick % 2 == 0) {
+  buttonToggle.innerHTML = "Favorite";
+  document.getElementById("favoriteButtonTN").className = "not_favorited";
+}
+else {
+  buttonToggle.innerHTML = "Favorited!";
+  document.getElementById("favoriteButtonTN").className = "favorited";
+}}
+
+function loadButtonState () {
+  var currentClick = localStorage.clickcount;
+  var buttonToggle = document.getElementById("favoriteButtonTN");
+  if(currentClick % 2 == 0) {
+    buttonToggle.innerHTML = "Favorited!";
+  document.getElementById("favoriteButtonTN").className = "favorited";
+}
+else {
+  buttonToggle.innerHTML = "Favorite";
+  document.getElementById("favoriteButtonTN").className = "not_favorited";
+}}
+*/
+
+  /*var like_control = document.getElementById("like_button");
+  like_control.classList.toggle("liked");
+  if (like_control.innerHTML === "Favorite") {
+    like_control.innerHTML = "Favorited!";
+    window.localStorage.setItem("Favorited!", like_control);
+    
+  }
+  else {
+    like_control.innerHTML = "Favorite";
+    window.localStorage.setItem("Favorite", like_control);
+  }
+}
+
+function Favorite() {
+  if (localStorage.clickcount) {
+    localStorage.clickcount = N
+  } else {
+    localStorage.clickcount = 1;
+  }
+  document.getElementById("test").innerHTML = localStorage.clickcount;
+}
+
+function getClick() {
+  
+}*/
+
+
+/*
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+      alert(localStorage.clickcount);
+
+    } else {
+      //never been liked...
+      sessionStorage.clickcount = 1;
+    }
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+  /*document.getElementById("like_button").innerHTML = "Favorited!";*/
+
